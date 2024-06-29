050b7d08706339e1a5206b5325ea5da3599c4053
Jadson Tavares <95153192+TSjadness@users.noreply.github.com>
2024-06-28 22:00:41 -0300

    add new update for page payments

--- src/app/confirmacao-de-pagamento/page.tsx
+++ src/app/confirmacao-de-pagamento/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import QRCode from "qrcode.react";
+import { useSearchParams } from "next/navigation";
+import { MdOutlineContentCopy } from "react-icons/md";
 
+const ConfirmationPage = () => {
+  const searchParams = useSearchParams();
+  const paymentMethod = searchParams.get("payment_method");
 
-
-const ConfirmationPage = ({payment_method }: { payment_method: string }) => {
   const [timeLeft, setTimeLeft] = useState(30 * 60);
   const [copied, setCopied] = useState(false);
+
+  // Supondo que "Seu código Pix aqui" seja o código Pix real
+  const pixCode = "Seu código Pix aqui";
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -25,7 +31,13 @@ const ConfirmationPage = ({payment_method }: { payment_method: string }) => {
   };
 
   const handleCopyPix = () => {
-    navigator.clipboard.writeText("Seu código Pix aqui");
+    navigator.clipboard.writeText(pixCode);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  const handleCopyPixInline = () => {
+    navigator.clipboard.writeText(pixCode);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };
@@ -37,12 +49,12 @@ const ConfirmationPage = ({payment_method }: { payment_method: string }) => {
           Seu pedido foi reservado!
         </h1>
 
-        {payment_method === "pix" && (
-          <>
-            <p className="text-gray-600 mb-4 text-center font-semibold text-xl">
-              Detalhes do pedido:
-            </p>
+        <p className="text-gray-600 mb-4 text-center font-semibold text-xl">
+          Detalhes do pedido:
+        </p>
 
+        {paymentMethod === "pix" ? (
+          <>
             <div className="flex items-center justify-center p-4">
               <ol className="font-semibold">
                 <li>1. Clique no botão abaixo para copiar o código</li>
@@ -76,14 +88,31 @@ const ConfirmationPage = ({payment_method }: { payment_method: string }) => {
               >
                 {copied ? "Copiado!" : "Copiar código Pix"}
               </button>
-              <div className="bg-gray-200 p-4 rounded">
-                <QRCode size={300} value="Seu código Pix aqui" />
+
+              <div className="mt-2 mb-4 flex items-center space-x-2">
+                <input
+                  type="text"
+                  readOnly
+                  value={pixCode}
+                  disabled
+                  className="border rounded px-10 py-1 text-gray-700"
+                />
+                <button
+                  onClick={handleCopyPixInline}
+                  className="bg-blue-500 hover:bg-blue-600 text-white rounded px-2 py-1"
+                >
+                  <MdOutlineContentCopy className="text-2xl" />
+                </button>
               </div>
+
+
+              <div className="bg-gray-200 p-4 rounded">
+                <QRCode size={300} value={pixCode} />
+              </div>
+              
             </div>
           </>
-        )}
-
-        {payment_method === "card" && (
+        ) : (
           <p className="text-gray-600 mb-4 text-center font-semibold text-xl">
             Sua reserva foi feita com sucesso através do pagamento com cartão.
           </p>
@@ -94,79 +123,3 @@ const ConfirmationPage = ({payment_method }: { payment_method: string }) => {
 };
 
 export default ConfirmationPage;
-
-// import React, { useState, useEffect } from "react";
-// import QRCode from "qrcode.react";
-
-// const ConfirmationPage = () => {
-//   const [timeLeft, setTimeLeft] = useState(30 * 60);
-//   const [copied, setCopied] = useState(false);
-
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       setTimeLeft((prevTimeLeft) => prevTimeLeft - 1);
-//     }, 1000);
-
-//     return () => clearInterval(timer);
-//   }, []);
-
-//   const formatTime = (time: any) => {
-//     const minutes = Math.floor(time / 60);
-//     const seconds = time % 60;
-//     return `${minutes.toString().padStart(2, "0")}:${seconds
-//       .toString()
-//       .padStart(2, "0")}`;
-//   };
-
-//   const handleCopyPix = () => {
-//     navigator.clipboard.writeText("código pix aqui");
-//     setCopied(true);
-//     setTimeout(() => setCopied(false), 2000); 
-//   };
-
-//   return (
-//     <div className="min-h-screen flex flex-col items-center justify-center bg-white">
-//       <div className=" w-full max-w-5xl animate__animated animate__fadeIn">
-//         <h1 className="text-4xl font-bold mb-4 text-center text-[#1EA230]">
-//           Seu pedido foi reservado!
-//         </h1>
-       
-//         <p className="text-gray-600 mb-4 text-center font-semibold text-xl">
-//           Detalhes do pedido:
-//         </p>
-        
-//         <div className="flex  items-center justify-center  p-4">
-//           <ol className=" font-semibold ">
-//             <li>1. Clique no botão abaixo para copiar o código</li>
-//             <li>2. Abra o aplicativo do seu banco ou instituição financeira e entre na opção PIX</li>
-//             <li>3. Na opção PIX copia e cola, insira o código copiado no passo 1.</li>
-//           </ol>
-//         </div>
-
-//         <div className="mb-4 text-center">
-//           <p className="text-red-700 font-semibold text-2xl">
-//             Tempo restante para o pagamento:
-//           </p>
-//           <p className="text-2xl font-mono text-red-700 font-bold ">
-//             {formatTime(timeLeft)}
-//           </p>
-//         </div>
-//         <div className="flex flex-col items-center">
-//           <button
-//             onClick={handleCopyPix}
-//             className={`mb-4 px-24 py-2 rounded text-white ${
-//               copied ? "bg-green-500 px-32 py-2 mb-4" : "bg-blue-500 hover:bg-blue-600 "
-//             } transition-colors duration-300`}
-//           >
-//             {copied ? "Copiado!" : "Copiar código Pix"}
-//           </button>
-//           <div className="bg-gray-200 p-4 rounded ">
-//             <QRCode size={300} value="Seu código Pix aqui" />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ConfirmationPage;
